bd878d1a35fb89c8c75cd1b8f66450a451ddbd42
Yashvardhan Shekhar <yashvardhan.shekhar@joshtechnologygroup.com>
2026-08-18 02:02:01 +0530

    [YS_A2_00]: Fixed eslint automation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,9 @@ import { Button } from '@mui/material';
 const Home = (user: User) => (
     <>
         {JSON.stringify(user)}
-        <Button color='secondary' variant='contained'>hello {user.name} </Button>
+        <Button color="secondary" variant="contained">
+            hello {user.name}{' '}
+        </Button>
     </>
 );
 export default Home;
--- src/theme/foundations/palette.ts
+++ src/theme/foundations/palette.ts
@@ -1,25 +1,25 @@
 import type { PaletteOptions } from '@mui/material/styles';
 
 export const palette: PaletteOptions = {
-  mode: 'light',
-  primary: {
-    light: '#0A9396',     
-    main: '#005F73',      
-    dark: '#00364A',      
-    contrastText: '#FFFFFF',
-  },
-  secondary: {
-    light: '#E0F2F1',     
-    main: '#00B4D8',      
-    dark: '#0077B6',
-    contrastText: '#FFFFFF',
-  },
-  background: {
-    default: '#F8F9FA',   
-    paper: '#FFFFFF',     
-  },
-  text: {
-    primary: '#0A2129',   
-    secondary: '#5A6A75', 
-  },
+    mode: 'light',
+    primary: {
+        light: '#0A9396',
+        main: '#005F73',
+        dark: '#00364A',
+        contrastText: '#FFFFFF',
+    },
+    secondary: {
+        light: '#E0F2F1',
+        main: '#00B4D8',
+        dark: '#0077B6',
+        contrastText: '#FFFFFF',
+    },
+    background: {
+        default: '#F8F9FA',
+        paper: '#FFFFFF',
+    },
+    text: {
+        primary: '#0A2129',
+        secondary: '#5A6A75',
+    },
 };
