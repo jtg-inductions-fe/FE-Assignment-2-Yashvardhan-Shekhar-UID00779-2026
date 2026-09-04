66eda83187e2e4011494ab817084498164238a86
Yashvardhan Shekhar <yashvardhan.shekhar@joshtechnologygroup.com>
2026-09-04 12:57:34 +0530

    [YS_A2_05]: Deleted unusedfile

--- src/components/Button/AddButton.component.tsx
+++
@@ -1,5 +0,0 @@
-import { ButtonProps } from '@mui/material';
-
-import { StyledAddButton } from './Button.styles';
-
-export const AddButton = (props: ButtonProps) => <StyledAddButton {...props} />;
