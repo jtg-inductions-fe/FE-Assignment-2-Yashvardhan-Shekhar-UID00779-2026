cffaad655e5e254ea81bcdebf742d1635b5142be
Yashvardhan Shekhar <yashvardhan.shekhar@joshtechnologygroup.com>
2026-09-04 12:08:52 +0530

    [YS_A2_01]: button padding

--- src/components/Button/Button.component.tsx
+++ src/components/Button/Button.component.tsx
@@ -1,5 +1,5 @@
 import { Button as MuiButton, ButtonProps } from '@mui/material';
 
 export const Button = (props: ButtonProps) => (
-    <MuiButton sx={{ p: 4 }} {...props} />
+    <MuiButton sx={{ py: 4, px: 8 }} {...props} />
 );
